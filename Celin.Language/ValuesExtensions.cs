1bb9a6ec78bafb51f2b3848f4f5e7d9305cb9faf
Finnur Bragason <fbragason@outlook.com>
2025-05-08 11:53:43 +0700

    .

--- Celin.Language/ValuesExtensions.cs
+++ Celin.Language/ValuesExtensions.cs
@@ -4,6 +4,17 @@ namespace Celin.Language;
 
 public static class ValuesExtensions
 {
+    public static bool IsSequential(this IEnumerable<int> source) =>
+        !source.Select((e, i) => e == i).Any(e => !e);
+    public static IEnumerable<int> GetTransposeOrder(
+        this IEnumerable<string> source,
+        IList<string> target) => source.Select(target.IndexOf);
+    public static IEnumerable<T?> Transpose<T>(
+        this IEnumerable<T> source, IList<int> order) =>
+        order.IsSequential() ? source : order.Select(ndx => source.ElementAtOrDefault(ndx));
+    public static IEnumerable<IEnumerable<T?>> Transpose<T>(
+        this IEnumerable<IEnumerable<T>> source, IList<int> order) =>
+        order.IsSequential() ? source : source.Select(r => order.Select(ndx => r.ElementAtOrDefault(ndx)));
     public static string ToSingle(this IEnumerable<IEnumerable<object?>> source)
         => source.FirstOrDefault()?.FirstOrDefault()?.ToString() ?? string.Empty;
     public static string ToList(this IEnumerable<IEnumerable<object?>> source)
